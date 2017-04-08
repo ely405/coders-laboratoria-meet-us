afdb02e66707076f8d63e995af4f3546e5c163c0
ely211 <gonzalesm405@gmail.com>
2017-04-07 23:53:15 -0500

    typing, agrega imagenes JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,34 +28,30 @@ function Student(nombre, urlFoto){
   this.urlFoto = urlFoto;
 }
 
-function loadImage(name, url) {
-  var divGrid = document.createElement("div");
-  divGrid.classList.add("grid");
-  var figure = document.createElement("figure");
-  var image = document.createElement("img");
-  image.setAttribute("src", url);
-  console.log(image);
-  var figcaption = document.createElement("figcaption");
-  var spanName = document.createElement("span");
-  console.log(divGrid);
-}
 
 window.addEventListener("load", function(){
-  // var   coder=[new Student("Fiorella Quispe", "../students/1.png"), new Student("Lourdes Vilchez", "../students/2.png"), new Student("Rosmery Maldonado", "../students/3.png"), new Student("Yessenia", "../assets/students/4.png"), new Student("Mirian Peralta", "../assets/students/5.png"), new Student("Elizabeth Condori", "../assets/students/6.png")];
+  var divContainer = document.getElementById("lima-prom6");
+  var   coder=[new Student("Fiorella Quispe", "assets/image/students/1.png"), new Student("Lourdes Vilchez", "assets/image/students/2.png"), new Student("Rosmery Maldonado", "assets/image/students/3.png"), new Student("Yessenia", "assets/image/students/4.png"), new Student("Mirian Peralta", "assets/image/students/5.png"), new Student("Elizabeth Condori", "assets/image/students/6.png")];
 
-  var coder=[{nombre: "Fiorella", urlFoto: "../students/1.png"}, {nombre: "Loudes", urlFoto: "../students/2.png"}]
-
-
-  console.log(coder);
   for (var i = 0; i < coder.length; i++) {
-    var divGrid = document.createElement("div");
-    divGrid.classList.add("grid");
-    var figure = document.createElement("figure");
-    var image = document.createElement("img");
-    image.setAttribute("src", coder.urlFoto );
-    console.log(image);
+    console.log(coder[i].urlFoto);
+    var img = document.createElement("img");
+    img.setAttribute("src", coder[i].urlFoto);
+    img.classList.add("coder-img");
+    console.log(img);
+    var span = document.createElement("span");
+    span.classList.add("text");
+    span.append(coder[i].nombre);
     var figcaption = document.createElement("figcaption");
-    var spanName = document.createElement("span");
-    console.log(divGrid);
+    figcaption.append(span);
+    var figure = document.createElement("figure");
+    figure.append(img);
+    figure.append(figcaption);
+    var div = document.createElement("div");
+    div.classList.add("grid");
+    div.append(figure);
+    console.log(div);
+    divContainer.append(div);
+    console.log(divContainer);
   }
 });
